3f885db01f586b8dfba04377b186ef112ee63f6b
skavipriya <35103577+skavipriya@users.noreply.github.com>
2018-01-20 22:21:56 +0530

    Create n number.c

---
+++ n number.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+	int n,i;
+	printf("enter the number");
+	scanf("%d",&n);
+	for(i=1;i<=5;i++)
+	 printf("the first five multipliers are:", n*i);
+	return 0;
+}
