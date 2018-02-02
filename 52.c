6a7bf676418445bde4af26a6c1ee5cf61f896fb3
skavipriya <35103577+skavipriya@users.noreply.github.com>
2018-02-02 16:11:02 +0530

    Create 52.c

---
+++ 52.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(void)
+{
+int a;
+printf("enter the number:");
+scanf("%d",&a);
+switch (a)
+{
+    case 1:
+    printf("one");
+    break ;
+    case 2:
+    printf("two");
+    break ;
+    case 3:
+    printf("three");
+    break ;
+}
+	
+return 0;
+}
