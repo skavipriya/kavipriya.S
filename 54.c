1ecdc345b9147ec74e15fb5b535b35524980a510
skavipriya <35103577+skavipriya@users.noreply.github.com>
2018-02-07 16:42:36 +0530

    Create 54.c

---
+++ 54.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+int main()
+{
+  int a;
+  scanf("%d",&a);
+  if(a%2!=0)
+  {
+    printf("nearest even number is: %d",a-1);
+  }
+  else
+  {
+    printf("nearest even number is: %d",a-2);
+  }
+  }
+	
+	
