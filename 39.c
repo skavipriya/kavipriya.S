29fdf97946b440cab4e9d928d5dad6803f284ed0
skavipriya <35103577+skavipriya@users.noreply.github.com>
2018-02-12 16:46:05 +0530

    Create 39.c

---
+++ 39.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+ int a[10],i,max;
+ for(i=0;i<10;i++)
+ {
+ scanf("%d",&a[i]);
+ }
+ max=a[0];
+ for(i=0;i<10;i++)
+{
+	if(a[i]>max)
+	max=a[i];
+}
+printf("The maximum number is:%d",max);
+return 0;
+}
+
