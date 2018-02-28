0b03f12141b006ff284a346959d68f2e9c9576d0
skavipriya <35103577+skavipriya@users.noreply.github.com>
2018-02-28 16:55:37 +0530

    Create 51.c

---
+++ 51.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() 
+{
+	int n,rem,rev=0;
+	printf("enter the number:");
+	scanf("%d",&n);
+	while(n!=0)
+	{
+		rem=n%10;
+		rev=(rev*10)+rem;
+		n=n/10;
+	}
+	n=rev;
+	while(n!=0)
+	{
+		rem=n%10;
+		printf("%d ",rem);
+		n=n/10;
+	}
+	return 0;
+
+}
