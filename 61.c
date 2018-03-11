229ae0a8e2cf9387c171481fe95e71e7cd4fb038
skavipriya <35103577+skavipriya@users.noreply.github.com>
2018-03-11 16:57:11 +0530

    Create 61.c

---
+++ 61.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+  char str[30];
+  int n,j;
+  printf("Enter the string and number:");
+  scanf("%s %d",&str,&n);
+  for(j=0;j<n;j++)
+  {
+    printf("%c",str[j]);
+    }
+  return 0;
+}
+
+    
+
+
+
