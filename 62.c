508e564383ab675f4c85036cd594d8be73326002
skavipriya <35103577+skavipriya@users.noreply.github.com>
2018-03-12 16:12:41 +0530

    Create 62.c

---
+++ 62.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+int main() 
+{
+	int i,n,count=0;
+	char a[10];
+	printf("Enter the numbers");
+	scanf("%s",&a);
+	for(i=0;i<n;i++)
+	
+		if(a[i]=='0'||a[i]=='1')
+		{
+			count=0;
+		}
+		else
+		{
+			count=1;
+			break;
+		}
+}
+	
+		if(a[i]==1)
+		{
+			printf("Yes");
+		}
+		return 0;
+}
