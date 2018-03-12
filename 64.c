f7e4eac30fffd1ce589cc1e72ce8f1a19f9cc649
skavipriya <35103577+skavipriya@users.noreply.github.com>
2018-03-12 16:40:39 +0530

    Create 64.c

---
+++ 64.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	int m,n,a;
+	scanf("%d",&m,&n);
+    a=m+n;
+    printf("%d",a);
+    if(a%2==0)
+    {
+    	printf("even");
+    }
+    else
+    {
+    	printf("odd");
+    }
+    
+}
+
+	
