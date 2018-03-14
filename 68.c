10df192ce018c61758d3e4f248ef769033463e4a
skavipriya <35103577+skavipriya@users.noreply.github.com>
2018-03-14 16:31:47 +0530

    Create 68.c

---
+++ 68.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int a;
+	printf("Enter the number");
+	scanf("%d",&a);
+	if(a%7==0)
+	{
+	printf("Yes");
+	}
+	else
+	{
+     printf("No");
+    }
+	return 0;
+}
+
