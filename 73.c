6f4181c633586a21005f32c8966e5a180caa93bb
skavipriya <35103577+skavipriya@users.noreply.github.com>
2018-03-16 16:42:46 +0530

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c;
+printf("enter the number");
+scanf("%d%d%d",&a,&b,&c);
+if((c<<a)||(c<b))
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return 0;
+}
